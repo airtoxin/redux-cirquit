da324a121c687e275d486d990a5e4e220d41f250
airtoxin <airtoxin@icloud.com>
2018-03-27 20:04:34 +0900

    Add source file

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Action } from "redux";
+
+export const CirquitActionType = "@cirquit/action";
+
+export interface CirquitReducer<State> {
+  (state: State): State;
+  name: string;
+}
+
+export interface CirquitAction<State> extends Action {
+  type: typeof CirquitActionType;
+  name: string;
+  reducer: CirquitReducer<State>;
+}
+
+export const createCirquitAction = <State>(
+  reducer: CirquitReducer<State>
+): CirquitAction<State> => ({
+  type: CirquitActionType,
+  name: reducer.name,
+  reducer
+});
+
+export const createCirquitReducer = <State>(initialState: State) => (
+  state: State = initialState,
+  action: CirquitAction<State>
+): State => {
+  switch (action.type) {
+    case CirquitActionType: {
+      return action.reducer(state);
+    }
+    default: {
+      return state;
+    }
+  }
+};
