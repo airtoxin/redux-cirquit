b6e4f226e51a3c2e031277ecb66460d08137a844
airtoxin <airtoxin@icloud.com>
2018-03-27 21:17:29 +0900

    Add test

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,56 @@
 import "jest";
+import * as cirquit from "./index";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(1 + 2).toBe(3);
+interface State {
+  counter: {
+    count: number;
+  };
+}
+
+const initialState: State = {
+  counter: {
+    count: 0
+  }
+};
+
+const noopReducer: cirquit.CirquitReducer<State> = state => state;
+
+const incrementAction = cirquit.createCirquitAction<State>(state => ({
+  counter: {
+    count: state.counter.count + 1
+  }
+}))
+
+describe("createCirquitAction", () => {
+
+  it("should return cirquitAction", () => {
+    expect(cirquit.createCirquitAction(noopReducer)).toEqual({
+      type: cirquit.CirquitActionType,
+      name: "noopReducer",
+      reducer: noopReducer
+    });
+  });
+
+  it("should return anonymous named cirquitAction", () => {
+    expect(cirquit.createCirquitAction(state => state).name).toBe("anonymous");
+  });
+
+});
+
+describe("createCirquitReducer's reducer", () => {
+
+  const reducer = cirquit.createCirquitReducer(initialState);
+
+  it("should return initialState when invoke with @@init action", () => {
+    expect(reducer(undefined, { type: "@@init" } as any)).toEqual(initialState);
+  });
+
+  it("should return reduced state when invoke with cirquitAction", () => {
+    expect(reducer(initialState, incrementAction)).toEqual({
+      counter: {
+        count: 1
+      }
+    });
+  });
+
 });
